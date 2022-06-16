623e9d7475f34d21cb364afe94566eb2ec05d8f6
olli <opelkonen@gmail.com>
2022-06-16 23:02:01 +0300

    js

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm ./wlook-back
-go build
-
---
+++ target_test/http.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+
+axios
+  .get('https://example.com/todos')
+  .then(res => {
+    console.log(`statusCode: ${res.status}`);
+    console.log(res);
+  })
+  .catch(error => {
+    console.error(error);
+  });
